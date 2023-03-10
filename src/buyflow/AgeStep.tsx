cad1232591c46d1f28177083a641d3f70932082f
Emad Dehnavi <emaddehnavi@Emads-MacBook-Pro-3.fritz.box>
2023-03-10 12:07:50 +0100

    feat(main): add age validations

--- src/buyflow/AgeStep.tsx
+++ src/buyflow/AgeStep.tsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row'
 import Form from 'react-bootstrap/Form'
 import Alert from 'react-bootstrap/Alert';
 import FormLabel from 'react-bootstrap/FormLabel'
+import { config } from '../utils';
 
 interface AgeStepProps {
   cb: (field: string, value: string) => void
@@ -17,6 +18,10 @@ export const AgeStep: React.FC<AgeStepProps> = (props) => {
     age ? setShowFormValidationFeedback("") : setShowFormValidationFeedback("Age field is required.")
     if(!age){
         return;
+    }
+    if(Number(age) < config.MIN_AGE || Number(age) > config.MAX_AGE) {
+      setShowFormValidationFeedback(`Our service is accessible to individuals between the ages of ${config.MIN_AGE} and ${config.MAX_AGE}`);
+      return;
     }
     props.cb('age', age)
   }
